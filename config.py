90cc6ea4889391aebeffac6ee24a21679e57be4e
robsenf <r.sendfeld@gmail.com>
2023-12-01 11:22:00 +0100

    added config file for common directories

---
+++ config.py
@@ -0,0 +1,6 @@
+# This is where the project files are located:
+file_directory = r'C:\Users\rsend\Documents\2.) Coding\CodeAcademy(repository)\P4_VS_Group_ECommerce\GitUploads'
+
+# This is the directory where CSVs get dropped in order to work with them:
+dropbox_directory = r'C:\Users\rsend\Documents\2.) Coding\CodeAcademy(repository)\P4_VS_Group_ECommerce\Dropbox'
+
